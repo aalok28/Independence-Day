6b52ace43fa59ef30851654494c9e17d162c4cc1
aalok28 <51107195+aalok28@users.noreply.github.com>
2020-04-21 14:45:27 +0530

    Final Commit

---
+++ js/flag.js
@@ -0,0 +1,15 @@
+$(function(){
+      $(".blink").typed({
+        strings: ["Happy Independence Day"],
+        typeSpeed: 0,
+        backSpeed: 0,
+        startDelay: 500,
+        backDelay: 500,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: true,
+        cursorChar: '|',
+        autoInsertCss: true,
+          
+      });
+  });
